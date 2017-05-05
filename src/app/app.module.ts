e20b8c666940162e9684756aa144e604c0c7261b
Chao Xie <chaoranxie@gmail.com>
2017-05-05 12:45:31 -0400

    Add article component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { ArticleComponent } from './article/article.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SidebarComponent
+    SidebarComponent,
+    ArticleComponent
   ],
   imports: [
     BrowserModule,
